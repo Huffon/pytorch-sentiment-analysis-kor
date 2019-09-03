e68c5d39c35b3a49e69c48ed7b658b7a1276ac3d
huffon <huffonism@gmail.com>
2019-09-03 14:18:11 +0900

    add docstring to utils.py'

--- utils.py
+++ utils.py
@@ -14,7 +14,16 @@ LABEL = td.LabelField(dtype=torch.float)
 
 
 def load_dataset(seed):
-    print(f'Load NSMC data and convert it to Dataframe . . .')
+    """
+    Load train and test dataset and split train dataset to make validation dataset.
+    And finally convert train, validation and test dataset to pandas DataFrame.
+    Args:
+        seed: (integer) used to equally split train dataset when 'train_test_split' is called
+
+    Returns:
+        (DataFrame) train, valid, test dataset converted to pandas DataFrame
+    """
+    print(f'Load NSMC data and convert it to DataFrame . . .')
 
     data_dir = Path().cwd() / 'data'
     train_txt = os.path.join(data_dir, 'train.txt')
@@ -32,10 +41,18 @@ def load_dataset(seed):
 
 
 def convert_to_dataset(data):
-    # remove id column
+    """
+    Pre-process input DataFrame and convert pandas DataFrame to torchtext Dataset.
+    Args:
+        data: (DataFrame)
+
+    Returns:
+        (Dataset) torchtext Dataset
+    """
+    # remove id column since it is not used to make torchtext Dataset
     data = data.iloc[:, 1:]
 
-    # convert integer label to string label
+    # convert integer label (0 / 1) to string label ('neg' / 'pos')
     data.loc[data['label'] == 0, ['label']] = 'neg'
     data.loc[data['label'] == 1, ['label']] = 'pos'
 
@@ -44,20 +61,25 @@ def convert_to_dataset(data):
     for idx, row in data.iterrows():
         if type(row.document) != str:
             missing_rows.append(idx)
-
     data = data.drop(missing_rows)
 
-    # convert each row to torchtext example
+    # convert each row of DataFrame to torchtext 'Example' which contains text and label attributes
     list_of_examples = [Example.fromlist(row.tolist(),
                                          fields=[('text', TEXT), ('label', LABEL)]) for _, row in data.iterrows()]
 
-    # make torchtext dataset using torchtext example list
+    # make torchtext 'Dataset' using torchtext 'Example' list
     dataset = Dataset(examples=list_of_examples, fields=[('text', TEXT), ('label', LABEL)])
 
     return dataset
 
 
 def build_vocab(data, vocab_size):
+    """
+    Build vocabulary used to convert input string to word index
+    Args:
+        data: (Dataset) torchtext 'train' Dataset used to build vocabulary
+        vocab_size: (integer) the size of vocabulary of your model
+    """
     print(f'\nBuild vocab . . .')
     TEXT.build_vocab(data, max_size=vocab_size)
     LABEL.build_vocab(data)
@@ -69,7 +91,20 @@ def build_vocab(data, vocab_size):
 
 
 def make_iter(train_data, valid_data, test_data, batch_size, vocab_size, device='cpu'):
-    # convert pandas Dataframe to torchtext dataset
+    """
+    Convert pandas DataFrame to torchtext Dataset and make iterator used to train and test
+    Args:
+        train_data: (DataFrame) pandas DataFrame used to make train iterator
+        valid_data: (DataFrame) pandas DataFrame used to make validation iterator
+        test_data: (DataFrame) pandas DataFrame used to make test iterator
+        batch_size: (integer) batch size used to make iterators
+        vocab_size: (integer) vocabulary size used to build vocabulary
+        device: (string) device name where train and test is run on
+
+    Returns:
+        (BucketIterator) train, valid, test iterator
+    """
+    # convert pandas DataFrame to torchtext dataset
     train_data = convert_to_dataset(train_data)
     valid_data = convert_to_dataset(valid_data)
     test_data = convert_to_dataset(test_data)
@@ -92,6 +127,16 @@ def make_iter(train_data, valid_data, test_data, batch_size, vocab_size, device=
 
 
 def epoch_time(start_time, end_time):
+    """
+    Calculate the time spent to train one epoch
+    Args:
+        start_time: (float) training start time
+        end_time: (float) training end time
+
+    Returns:
+        (int) elapsed_mins and elapsed_sec spent for one epoch
+
+    """
     elapsed_time = end_time - start_time
     elapsed_mins = int(elapsed_time / 60)
     elapsed_secs = int(elapsed_time - (elapsed_mins * 60))
